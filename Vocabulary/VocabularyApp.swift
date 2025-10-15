0c8ee6e8f05c5789d91dbc17fdfc25ec188fe87f
stepan-bezhuk-axon <sbezhuk@axon.dev>
2025-10-15 13:16:07 +0300

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Stepan Bezhuk on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
